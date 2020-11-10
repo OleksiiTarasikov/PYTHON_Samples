8413931db5d8a3c021dffd510495b722089d5f64
AlexeyTarasikov <AlexeyTarasikov@gmail.com>
2020-11-10 22:09:13 +0200

    first1 commit

--- PDF Tool Design.py
+++ PDF Tool Design.py
@@ -10,3 +10,30 @@ class Pdf_Tool(object):
         frame.grid()
         self.file_path = ""
         self.options = ["Text", "Docx", "Picture", "Pdf"]
+
+        self.selection_frame = LabelFrame(root, text,="PDF Convertion Tool", heignt=100, width=100)
+        self.selection_frame_grid(column=0, row=0, padx=10, pady=10)
+
+        self.file_label = Label(self, selection_frame, text="File: ")
+        self.
+        self.
+        self.
+        self.
+        self.
+
+        self.
+        self.
+        self.
+        self.
+        self.
+        self.
+        self.
+
+        self.result_label = Label(self. selection_frame, text="")
+        self.result_label.grid(column=0, row=2, columnspan=3)
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title("PDF Tool")
+    Pdf_Tool(root)
+    root.mainloop()
