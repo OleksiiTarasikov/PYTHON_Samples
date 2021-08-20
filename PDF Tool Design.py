a202631219ff87524151285820fa8ce92259bbba
Alexey Tarasikov <40866807+AlexeyTarasikov@users.noreply.github.com>
2021-08-20 22:35:30 +0300

    Update PDF Tool Design.py

--- PDF Tool Design.py
+++ PDF Tool Design.py
@@ -4,7 +4,7 @@ from tkinter.ttk import *
 from tkinter import ttk
 from tkinter import filedialog as fd
 
-class Pdf_Tool(object):
+class Pdf_Tool (object):
     def __init__(self, master):
         frame = Frame(master)
         frame.grid()
